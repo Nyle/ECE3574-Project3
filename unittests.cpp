b4dd61e6ab43310bb9aac1f62f3d8d49f47ea6b6
Nyle Rodgers <nyle.rodgers@gmail.com>
2017-04-21 20:47:12 -0400

    Add some tests for parsing from JSON

--- unittests.cpp
+++ unittests.cpp
@@ -2,34 +2,97 @@
 #define CATCH_CONFIG_COLOUR_NONE
 #include "catch.hpp"
 
-// IMPORTANT NOTE:
-// These are just a few examples from my solution and **should be removed**.
-// Depending on your code design your class and functions names would differ
+#include "scene.hpp"
+#include "json_format_error.hpp"
 
-#include "geometry.hpp"
+#include <QByteArray>
+#include <QJsonArray>
+#include <QJsonParseError>
+#include <QJsonDocument>
+#include <QJsonObject>
 
-TEST_CASE( "Test Basic Geometry: Vec3d ", "[geometry]" ) {
+QJsonDocument strToJsonDocument(std::string str) {
+    QJsonParseError error;
+    QJsonDocument doc = QJsonDocument::fromJson(QByteArray::fromStdString(str),
+                                                &error);
+    REQUIRE(error.error == QJsonParseError::NoError);
+    return doc;
+}
 
-  Vec3d a(2,0,0), b(0,4,0);
-  
-  REQUIRE(a.x == Approx(2));
-  REQUIRE(a.y == Approx(0));
-  REQUIRE(a.z == Approx(0));
+QJsonObject strToJsonObject(std::string str) {
+    QJsonDocument doc = strToJsonDocument(str);
+    REQUIRE(doc.isObject());
+    return doc.object();
+}
 
-  REQUIRE(b.x == Approx(0));
-  REQUIRE(b.y == Approx(4));
-  REQUIRE(b.z == Approx(0));
+QJsonArray strToJsonArray(std::string str) {
+    QJsonDocument doc = strToJsonDocument(str);
+    REQUIRE(doc.isArray());
+    return doc.array();
+}
 
-  double adotb = dot(a,b);
-  REQUIRE(adotb == Approx(0));
+TEST_CASE("Test reading JSON inputs into sphere object", "[scene]") {
+    REQUIRE_NOTHROW(Sphere(strToJsonObject("{\"center\": "
+                                           "{ \"x\": 0, \"y\": 0, \"z\": 0},"
+                                           "\"color\": "
+                                           "{\"b\": 0, \"g\": 0, \"r\": 255},"
+                                           "\"lambert\": 1,"
+                                           "\"radius\": 1,"
+                                           "\"type\": \"sphere\""
+                                           "}")));
+}
 
-  Vec3d anorm = norm(a);
-  REQUIRE(anorm.x == Approx(1));
-  REQUIRE(anorm.y == Approx(0));
-  REQUIRE(anorm.z == Approx(0));
+TEST_CASE("Test reading JSON inputs into Plane object", "[scene]") {
+    REQUIRE_NOTHROW(Plane(strToJsonObject("{\"center\": "
+                                           "{ \"x\": 0, \"y\": 0, \"z\": 0},"
+                                           "\"normal\": "
+                                           "{ \"x\": 1, \"y\": 0, \"z\": 0},"
+                                           "\"color\": "
+                                           "{\"b\": 0, \"g\": 0, \"r\": 255},"
+                                           "\"lambert\": 1,"
+                                           "\"type\": \"plane\""
+                                           "}")));
+}
 
-  Vec3d bnorm = norm(b);
-  REQUIRE(bnorm.x == Approx(0));
-  REQUIRE(bnorm.y == Approx(1));
-  REQUIRE(bnorm.z == Approx(0));
+
+TEST_CASE("Test reading JSON inputs into Camera object", "[scene]") {
+    REQUIRE_NOTHROW(Camera(strToJsonObject("{\"center\": "
+                                           "{ \"x\": 0, \"y\": 0, \"z\": 0},"
+                                           "\"focus\": 10,"
+                                           "\"normal\": "
+                                           "{ \"x\": 0, \"y\": 0, \"z\": 1},"
+                                           "\"resolution\": [0.01, 0.01],"
+                                           "\"size\": [256,256]"
+                                           "}")));
+}
+
+TEST_CASE("Test reading JSON inputs into Light object", "[scene]") {
+    REQUIRE_NOTHROW(Light(strToJsonObject("{\"location\": "
+                                          "{ \"x\": 0, \"y\": 0, \"z\": 0},"
+                                           "\"intensity\": 3"
+                                           "}")));
+}
+
+
+TEST_CASE("Test reading JSON inputs into a Scene object", "[scene]") {
+    REQUIRE_NOTHROW(Scene(strToJsonObject("{\"camera\":"
+                                          "{\"center\": "
+                                          "{ \"x\": 0, \"y\": 0, \"z\": 0},"
+                                          "\"focus\": 10,"
+                                          "\"normal\": "
+                                          "{ \"x\": 0, \"y\": 0, \"z\": 1},"
+                                          "\"resolution\": [0.01, 0.01],"
+                                          "\"size\": [256,256]},"
+                                          "\"lights\":"
+                                          "[{\"intensity\": 1,"
+                                          "\"location\": "
+                                          "{ \"x\": 5, \"y\": -5, \"z\": 0}}],"
+                                          "\"objects\":"
+                                          "[{\"center\": "
+                                          "{ \"x\": 0, \"y\": 0, \"z\": 5},"
+                                          "\"color\": "
+                                          "{\"b\": 0, \"g\": 0, \"r\": 255},"
+                                          "\"lambert\": 1,"
+                                          "\"radius\": 1,"
+                                          "\"type\": \"sphere\"}]}")));
 }
