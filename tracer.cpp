e0071c83ceb8018853f2c9b41f42a7f44fa0bd25
Nyle Rodgers <nyle.rodgers@gmail.com>
2017-04-24 01:01:58 -0400

    shuffle around tracing tasks

--- tracer.cpp
+++ tracer.cpp
@@ -1,60 +1,23 @@
 #include "tracer.hpp"
 
 #include "scene.hpp"
+#include <iostream>
+#include <cmath>
 #include <limits>
-
-Vec3D focusPoint(Camera c) {
-    return c.center - c.normal.norm() * c.focus;
-}
-
-// Gives `vec' rotated around `around' by `theta' radians. See
-// https://en.wikipedia.org/wiki/Rotation_matrix#Rotation_matrix_from_axis_and_angle
-Vec3D rotate(Vec3D vec, Vec3D around, double theta) {
-    around = around.norm();
-    return Vec3D(// X
-                 (cos(theta) + around.x * around.x * (1 - cos(theta))) * vec.x +
-                 (around.x * around.y * (1 - cos(theta)) -
-                  around.z * sin(theta)) * vec.y +
-                 (around.x * around.z * (1 - cos(theta)) +
-                  around.y * sin(theta)) * vec.z,
-                 // Y
-                 (around.y * around.x * (1 - cos(theta)) +
-                  around.z * sin(theta)) * vec.x + 
-                 (cos(theta) + around.y * around.y * (1 - cos(theta))) * vec.y +
-                 (around.y * around.z * (1 - cos(theta)) -
-                  around.x * sin(theta)) * vec.z,
-                 // Z
-                 (around.z * around.x * (1 - cos(theta)) -
-                  around.y * sin(theta)) * vec.x +
-                 (around.z * around.y * (1 - cos(theta)) +
-                  around.x * sin(theta)) * vec.y +
-                 (cos(theta) + around.z * around.z * (1 - cos(theta))) * vec.z);
-}
-
-const Vec3D defaultCameraNormal = Vec3D(0, 0, 1);
-const Vec3D defaultCameraVert = Vec3D(0, 1, 0);
-const Vec3D defaultCameraHoriz = Vec3D(1, 0, 0);
 
 // Get the location of the pixel at coordinate (i, j)
 const Vec3D pixelPoint(Camera c, size_t i, size_t j) {
-    Vec3D rotatedVert = defaultCameraVert;
-    Vec3D rotatedHoriz = defaultCameraHoriz;
-    Vec3D axis = crossProduct(defaultCameraNormal, c.normal);
-    if (axis.mag2() != 0) {
-        double angle = acos(defaultCameraNormal.norm() * c.normal.norm());
-        rotatedVert = rotate(defaultCameraVert, axis, angle);
-        rotatedHoriz = rotate(defaultCameraHoriz, axis, angle);
-    }
-
-    return c.center + (i - 0.5 * c.sizex) * c.resolutionx * rotatedHoriz +
-        (j - 0.5 * c.sizey) * c.resolutiony * rotatedVert;
+    return c.center + (i - 0.5 * c.sizex) * c.resolutionx * c.xDir +
+        (j - 0.5 * c.sizey) * c.resolutiony * c.yDir;
 }
 
-Color trace(Scene * s, size_t i, size_t j) {
-    Ray r = Ray(focusPoint(s->camera), pixelPoint(s->camera, i, j));
-    double closestDistance = std::numeric_limits<double>::infinity();
+// Finds the object in `s' which intersects `r' closest to its start. `closest'
+// will be the set to the found object, and `closestDistance' will be set to
+// the distance from the start of `r' where the intersection happens.
+void getIntersection(Scene * s, Ray r, SceneObject * & closest,
+                     double & closestDistance) {
+    closestDistance = std::numeric_limits<double>::infinity();
     double tmp;
-    SceneObject * closest;
     
     for (SceneObject * obj : s->objects) {
         tmp = obj->intersect(r);
@@ -63,10 +26,50 @@ Color trace(Scene * s, size_t i, size_t j) {
             closest = obj;
         }
     }
+    return;
+}
+
+// Find if there is another object beside `exclude' between the start of the ray
+// and `stopAt' along the ray
+bool isShadowed(Scene * s, Ray r, SceneObject * exclude, double stopAt) {
+    for (SceneObject * obj : s->objects) {
+        if (obj == exclude) continue;
+        if (obj->intersect(r) <= stopAt) return true;
+    }
+    return false;
+}
+
+Color trace(Scene * s, size_t i, size_t j) {
+    Color result = VOID_COLOR;
+    Ray r = Ray(s->camera.focusPoint, pixelPoint(s->camera, i, j));
+    double closestDistance;
+    SceneObject * closest;
+    getIntersection(s, r, closest, closestDistance);
 
     if (closestDistance == std::numeric_limits<double>::infinity()) {
-        return VOID_COLOR;
+        return result;
     }
 
-    return closest->color;
+    Vec3D intersectionPt = r.pointAt(closestDistance);
+    Vec3D intersectionNormal = closest->normalAt(intersectionPt);
+
+    for (auto light : s->lights) {
+        r = Ray(intersectionPt, light.location);
+        double cameraPlaneIntersectionDist = r.dir * s->camera.normal == 0 ?
+            std::numeric_limits<double>::infinity() :
+            ((s->camera.center - r.start) * s->camera.normal) /
+            (r.dir * s->camera.normal);
+        
+        if (!isShadowed(s, r, closest,
+                             // We only care about ones that are between the
+                             // light and the intersection point, and are also
+                             // on the far side of the camera plane
+                             fmin((intersectionPt - light.location).mag(),
+                                  cameraPlaneIntersectionDist))) {
+            double scale = intersectionNormal * r.dir * closest->lambert;
+            if (scale >= 0) result += scale * light.intensity * closest->color;
+        }
+    }
+
+    return result;
 }
